52a8d7bf363583e0fee7c8db9780477185549066
vishnutha1605 <vishnuthamyakala07@gmail.com>
2025-01-29 05:43:31 -0800

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 6;
+        int result = num1 / num2;
+        System.out.println("the result of division is:" + result);
+    }
+}
